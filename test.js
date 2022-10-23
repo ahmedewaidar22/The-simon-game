619beb03f9070f95f93c3a9f7a22cced05693e24
ahmedewaidar22 <50559116+ahmedewaidar22@users.noreply.github.com>
2022-10-23 15:11:18 +0200

    uploading the simon game

---
+++ test.js
@@ -0,0 +1,63 @@
+ flag = true
+ gamebox=[]
+colorbutton=["green","red","yellow","blue"]
+$("body").keypress(function (e) { 
+    console.log(e.key);
+    if(e.key=="a" && flag==true){
+
+        nextSequence();
+        //  flag=false
+    }
+});
+
+$(".btn").click(function () { 
+    var Id=$(this).attr("id");
+    clickedGameBox.push(Id)
+    // animation()
+    console.log(Id)
+    animation(Id)
+    playSound(Id)
+    checkAnswer(clickedGameBox.length-1)
+});
+
+function checkAnswer(lenghtclickedbox){
+if(clickedGameBox[lenghtclickedbox]==gamebox[lenghtclickedbox]){
+if(clickedGameBox.length==gamebox.length){
+    setTimeout(function () {
+        nextSequence();
+      }, 1000);}
+}else{
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    startOver();
+}
+
+}
+
+function nextSequence(){
+    clickedGameBox=[]
+    var i=Math.floor(Math.random() * 4)
+     console.log(colorbutton[i])
+    gamebox.push( colorbutton[i]);
+    // console.log(gamebox[i]+"--------->")}
+    animation(colorbutton[i])
+    playSound(colorbutton[i])
+
+}
+
+function animation(sound){
+    console.log("sound")
+$("."+sound).fadeOut(100).fadeIn(100);
+}
+function playSound(name){
+    console.log(name)
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+// $("button").attr(h, value);
